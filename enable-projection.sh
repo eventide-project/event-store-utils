087f87e2670171bc331befc4d2ccfc8135112df9
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-08-14 10:15:00 -0500

    Add environment variable for which port to enable projections on, which defaults to 2113.

--- enable-projection.sh
+++ enable-projection.sh
@@ -4,13 +4,16 @@ set -u
 
 name="$1"
 
+port=${PORT-2113}
+
 echo
 echo "Enabling $name"
 echo "- - -"
+echo "Port $port"
 
 if [[ ! "$name" == "users" ]]; then
   name="%24$name"
-  curl -i -X POST -d '' http://127.0.0.1:2113/projection/${name}/command/enable -u admin:changeit
+  curl -i -X POST -d '' http://127.0.0.1:${port}/projection/${name}/command/enable -u admin:changeit
 else
   echo "$name is enabled by default. Skipping."
 fi
