4e847ef4e9269056f4625d54cff3a62b17f01b18
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-03 12:51:43 -0600

    Host can be specified when enabling projections

--- enable-projection.sh
+++ enable-projection.sh
@@ -5,14 +5,15 @@ set -u
 name="$1"
 
 port=${EVENT_STORE_PORT:-2113}
+host=${EVENT_STORE_HOST:-localhost}
 
 echo
-echo "Enabling $name (Port: $port)"
+echo "Enabling $name (Host: $host, Port: $port)"
 echo "- - -"
 
 if [[ ! "$name" == "users" ]]; then
   name="%24$name"
-  curl -i -X POST -d '' http://127.0.0.1:${port}/projection/${name}/command/enable -u admin:changeit
+  curl -v -i -X POST -d '' http://${host}:${port}/projection/${name}/command/enable -u admin:changeit
 else
   echo "$name is enabled by default. Skipping."
 fi
